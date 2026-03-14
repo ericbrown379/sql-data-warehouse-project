0347b9535c4650df7eb49ee6677b6576a116c00e
Eric Brown <ericbrown379@gmail.com>
2026-03-14 00:01:36 -0500

    Initialize DataWarehouse database with schemas
    This script initializes the DataWarehouse database by dropping it if it exists and recreating it with three schemas: bronze, silver, and gold.

---
+++ scripts/init_database.sql
@@ -0,0 +1,29 @@
+USE master;
+GROUP BY
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+
+-- Create Schema
+CREATE SCHEMA bronze;
+GO
+
+
+CREATE SCHEMA silver;
+GO
+
+
+CREATE SCHEMA gold;
+GO;
